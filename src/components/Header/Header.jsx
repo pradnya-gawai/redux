bd4e64d448fdef51704ff6bdbea7f818de344e8c
Pradnya Gawai <pradnya.gawai@mewurk.com>
2021-11-13 21:37:37 +0530

    header component add

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <span className="header">🎬 Movies Hub 🎬</span>
+    </div>
+  );
+}
+
+export default Header;
